1bc99caf77d340b733c7b403592e8e6db17e9190
Thien Ngan Doan <46005184+ngandn18@users.noreply.github.com>
2022-09-09 11:38:43 -0500

    Update project.py
    Create the most important function create_tree(...)

--- project.py
+++ project.py
@@ -19,3 +19,55 @@ def is_solution(node, needed):
     """
     return needed == node.container_1 or needed == node.container_2 
 
+
+def create_tree(storage, capacity_1, capacity_2, needed):
+    """
+    Create the first node of the tree
+    Create the first value of container list
+    Create the tree and find if the solution nodes exist in the tree
+    Return the results dictionary with the details of the path from root to the node having the needed value and the complete tree.
+    """    
+    tree = {}
+    lst_containers = []
+    tree[0] = Node(0, 0, storage, 0, 0, [0], 0, 0)
+    lst_containers.append((0,0))
+    result = []
+
+    control = 1
+    start = 0
+
+    while control > 0:
+        total = max(tree.keys())
+
+        for k in range(start, total + 1):
+            if k not in result:
+                pseudo_nodes = tree[k].activate_rules(storage, capacity_1, capacity_2)
+                for node in pseudo_nodes:
+                    if node['containers'] not in lst_containers:
+                        # Create id for new node
+                        id = max(tree.keys()) + 1
+                        # Create path for new node
+                        path = tree[node['parent_id']].path.copy()
+                        path.append(id)
+                        # Create level for new node
+                        level = tree[node['parent_id']].level + 1
+                        # Create new node 
+                        tree[id] = Node(id, node['parent_id'], node['n_storage'], node['containers'][0], node['containers'][1], path, node['rule'], level)
+                        # Update lst_containers for further checking new Node
+                        lst_containers.append(node['containers'])
+                        # Check if Node is the result: is_solution(tree[id], needed), add this Node to the result list
+                        if is_solution(tree[id], needed):
+                            result.append(id)
+                
+        # Print the list of nodes for debug
+        # for key in range(total+1, max(tree.keys())+1):
+        #     print(tree[key])
+
+        start = total + 1
+        control = max(tree.keys()) - total
+        # stop creating more nodes when there are 2 solutions
+        if len(result) == 2:
+            break
+
+    return result, tree
+
