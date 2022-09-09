193029c112a79786a965c9ebf1c1286946d753ca
Thien Ngan Doan <46005184+ngandn18@users.noreply.github.com>
2022-09-09 11:36:06 -0500

    Update project.py
    Create the mini set of 6 rules.

---
+++ project.py
@@ -0,0 +1,15 @@
+from mynode import Node, get_vars
+
+# Rule 0 represent the initiation of the tree
+rules = [
+        'Begin: 2 containers are empty',
+        'Move oil from the container 2 into container 1 - R1',
+        'Move oil from the storage into container 1 - R2',
+        'Move oil from the container 1 into container 2 - R3',
+        'Move oil from the storage into container 2 - R4',
+        'Move oil from the container 1 into the storage - R5',
+        'Move oil from the container 2 into the storage - R6'
+    ]
+
+
+
